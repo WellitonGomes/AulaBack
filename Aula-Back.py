fe9f1c80d97fa92d3b0cb82c3042e5ff7feda7e3
Welliton da Silva Gomes <137785960+WellitonGomes@users.noreply.github.com>
2024-04-19 11:13:18 -0300

    Add files via upload

---
+++ Aula-Back.py
@@ -0,0 +1,12 @@
+#receber um input de valor string
+string1 = str(input("Digite a string1"))
+#imprimir a string1
+print(string1)
+#receber um input de valor string
+string2 = str(input("Digite a string2"))
+#imprimir a string2
+print(string2)
+#concatenar estes valores string em uma nova variavel
+string3 = string1 + string2
+#imprimir a nova variavel
+print(string3)
